ad4441d9a8bc4ac5808ce7871c82790d023dfe9e
Oliver Sõro <oliver.s6ro@gmail.com>
2021-04-11 11:21:49 +0300

    Play with fp-ts Option, map, flatten and Chain (flatmap).

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,86 @@
 /**
- * https://rlee.dev/writing/practical-guide-to-fp-ts-part-1
- * Learning to use fp-ts pipe, flow
+ * https://rlee.dev/writing/practical-guide-to-fp-ts-part-2
+ * Learning to use fp-ts Option, Map, Chain
  */
 
 import { pipe } from 'fp-ts/pipeable';
 import { flow } from 'fp-ts/lib/function';
+import * as O from 'fp-ts/Option';
 
-function addOne(num: number): number {
-  return num + 1;
+const foo1 = {
+  bar: 'hello',
+};
+
+interface Foo {
+  bar: string;
 }
 
-function mulTwo(num: number): number {
-  return num * 2;
+const foo2 = {
+  bar: 'hello',
+} as Foo | undefined;
+
+const fooStr1 = pipe(foo1, (f) => f.bar);
+const fooStr2 = pipe(foo2, (f) => f?.bar);
+
+const opt1: O.Option<string> = pipe(
+  foo2,
+  O.fromNullable,
+  O.map(({ bar }) => bar),
+);
+const opt2: O.Option<string> = pipe(
+  undefined,
+  O.fromNullable,
+  O.map(({ bar }) => bar),
+);
+
+interface Fizz {
+  buzz: string;
+}
+interface Fuzz {
+  bar?: Fizz;
 }
 
-function toStr(num: number): string {
-  return `${num}`;
-}
-
-function concat(
-  a: number,
-  transformer: (a: number) => string,
-): [number, string] {
-  return [a, transformer(a)];
-}
-
-const pipeResult = pipe(1, addOne, mulTwo, toStr);
-const flowResult1 = pipe(1, flow(addOne, mulTwo, toStr));
-const flowResult2 = flow(addOne, mulTwo, toStr)(1);
-const concatResult1 = concat(1, (n) => pipe(n, addOne, mulTwo, toStr)); // not ideal
-const concatResult2 = concat(1, flow(addOne, mulTwo, toStr));
+const foo3 = { bar: undefined } as Fuzz | undefined;
+const fooStr3 = pipe(foo3, (f: any) => f?.bar?.buzz);
+const opt3: O.Option<O.Option<string>> = pipe(
+  foo3,
+  O.fromNullable,
+  O.map(({ bar }) =>
+    pipe(
+      bar,
+      O.fromNullable,
+      O.map(({ buzz }) => buzz),
+    ),
+  ),
+);
+const opt4: O.Option<string> = pipe(
+  foo3,
+  O.fromNullable,
+  O.map(({ bar }) =>
+    pipe(
+      bar,
+      O.fromNullable,
+      O.map(({ buzz }) => buzz),
+    ),
+  ),
+  O.flatten,
+);
+const opt5: O.Option<string> = pipe(
+  foo3,
+  O.fromNullable,
+  O.map(({ bar }) => bar),
+  O.chain(
+    flow(
+      O.fromNullable,
+      O.map(({ buzz }) => buzz),
+    ),
+  ),
+);
 
 console.log(
-  `Pipe ${pipeResult} | Flow ${flowResult1}, ${flowResult2} | Concat ${concatResult1} - ${concatResult2}`,
+  `Foo str\n\t${fooStr1}\n\t${fooStr2}\n\t${fooStr3} \nOpts\n\t${JSON.stringify(
+    opt1,
+  )}\n\t${JSON.stringify(opt2)}\n\t${JSON.stringify(opt3)}\n\t${JSON.stringify(
+    opt4,
+  )}\n\t${JSON.stringify(opt5)}`,
 );

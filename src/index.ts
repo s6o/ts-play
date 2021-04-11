968ef5a0aa07483d863b0b09b4f5477882a7fc09
Oliver Sõro <oliver.s6ro@gmail.com>
2021-04-11 10:39:19 +0300

    Play with fp-ts pipe and flow.

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,36 @@
 /**
- * Should I use fp-ts Task?
- * https://dev.to/anthonyjoeseph/should-i-use-fp-ts-task-h52
+ * https://rlee.dev/writing/practical-guide-to-fp-ts-part-1
+ * Learning to use fp-ts pipe, flow
  */
 
-import fetch from 'node-fetch';
-import { Response } from 'node-fetch';
-import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/pipeable';
-import * as T from 'fp-ts/Task';
-import * as TE from 'fp-ts/TaskEither';
+import { flow } from 'fp-ts/lib/function';
 
-
-function jsFetch(): Promise<any> {
- return fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch((err: any) => {
-    console.error(err.mesage)
-  });
+function addOne(num: number): number {
+  return num + 1;
 }
 
-const safeFetch: T.Task<void> = pipe(
-  TE.tryCatch(
-    (): Promise<Response> => fetch('https://jsonplaceholder.typicode.com/todos/1'),
-    E.toError,
-  ),
-  TE.chain((r: Response) => TE.tryCatch((): Promise<unknown> => r.json(), E.toError)),
-  T.map(E.fold(console.error, console.log)),
+function mulTwo(num: number): number {
+  return num * 2;
+}
+
+function toStr(num: number): string {
+  return `${num}`;
+}
+
+function concat(
+  a: number,
+  transformer: (a: number) => string,
+): [number, string] {
+  return [a, transformer(a)];
+}
+
+const pipeResult = pipe(1, addOne, mulTwo, toStr);
+const flowResult1 = pipe(1, flow(addOne, mulTwo, toStr));
+const flowResult2 = flow(addOne, mulTwo, toStr)(1);
+const concatResult1 = concat(1, (n) => pipe(n, addOne, mulTwo, toStr)); // not ideal
+const concatResult2 = concat(1, flow(addOne, mulTwo, toStr));
+
+console.log(
+  `Pipe ${pipeResult} | Flow ${flowResult1}, ${flowResult2} | Concat ${concatResult1} - ${concatResult2}`,
 );
-
-
-(async () => {
-
-  await jsFetch();
-  await safeFetch();
-
-})();
-
